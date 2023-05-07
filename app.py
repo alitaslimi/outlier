850974d7d1d9791d8d3963d9cc265b78d58fa572
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-05-07 13:47:13 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import PIL
 from datetime import date, datetime, timedelta
-from shroomdk import ShroomDK
-from pathlib import Path
 
 # Page Favicon
 favicon = PIL.Image.open('favicon.png')
@@ -18,6 +16,7 @@ st.set_page_config(page_title='Outlier - Blockchain Analytics', page_icon=favico
 theme_plotly = 'streamlit'
 queries = pd.read_csv('data/queries.csv')
 charts = pd.read_csv('data/charts.csv')
+week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 # Filters
 c1, c2 = st.columns(2)
@@ -31,78 +30,64 @@ with c1:
 with c2:
     option_metrics = st.selectbox(
         '**Metric**',
-        options=queries.query("Segment == @st.session_state.option_segments")['Metric'].unique(),
+        options=queries.query("Segment == @option_segments")['Metric'].unique(),
         key='option_metrics'
     )
 
 option_blockchains = st.multiselect(
     '**Blockchains**',
-    options=queries.query("Segment == @st.session_state.option_segments & Metric == @st.session_state.option_metrics")['Blockchain'].unique(),
-    default=queries.query("Segment == @st.session_state.option_segments & Metric == @st.session_state.option_metrics")['Blockchain'].unique(),
+    options=queries.query("Segment == @option_segments & Metric == @option_metrics")['Blockchain'].unique(),
+    default=queries.query("Segment == @option_segments & Metric == @option_metrics")['Blockchain'].unique(),
     key='option_blockchains'
 )
 
 # Data
-data_file = f"data/{st.session_state.option_segments.lower()}_daily_{st.session_state.option_metrics.lower().replace(' ', '_')}.csv"
+data_file = f"data/{option_segments.lower()}_{option_metrics.lower().replace(' ', '_')}_daily.csv"
 df = pd.read_csv(data_file)
 
-if df['Date'].iloc[0] >= str(date.today() - timedelta(2)):
-    df = df.query("Blockchain == @st.session_state.option_blockchains")
+if df['Date'].iloc[0] >= str(date.today() - timedelta(1)) and df.loc[df['Date'] == df['Date'].iloc[0], 'Blockchain'].unique().size == df['Blockchain'].unique().size:
+    df = df.query("Blockchain == @option_blockchains")
 
 else:
-    pass
-    API_KEY = st.secrets["API_KEY"]
-    sdk = ShroomDK(API_KEY)
+    query_result = pd.DataFrame()
+    for blockchain in option_blockchains:
+        if df[df['Blockchain'] == blockchain]['Date'].iloc[0] < str(date.today() - timedelta(2)):
+            query_id = queries.query("Segment == @option_segments & Metric == @option_metrics & Blockchain == @blockchain")['Query'].iloc[0]
+            query_result = pd.read_json(f"https://api.flipsidecrypto.com/api/v2/queries/{query_id}/data/latest")
+            query_result['Blockchain'] = blockchain
+            query_result['Date'] = query_result['Date'].dt.strftime('%Y-%m-%d')
+            df = pd.concat([query_result[~query_result['Date'].isin(df[df['Blockchain'] == blockchain]['Date'])], df]).sort_values(['Date', 'Blockchain'], ascending=[False, True]).reset_index(drop=True)
 
-    dft = pd.DataFrame()
-    for blockchain in st.session_state.option_blockchains:
-        sql = Path(f"queries/{blockchain.lower()}_{st.session_state.option_segments.lower()}_daily_{st.session_state.option_metrics.lower().replace(' ', '_')}.sql").read_text()
-        result_list = []
-        for i in range(1,11):
-            query_result = sdk.query(sql, page_size=100000, page_number=i)
-            if query_result.run_stats.record_count == 0:  
-                break
-            else:
-                result_list.append(query_result.records)
-        result_df = pd.DataFrame()
-        for idx, each_list in enumerate(result_list):
-            if idx == 0:
-                result_df = pd.json_normalize(each_list)
-            else:
-                result_df = pd.concat([dft, pd.json_normalize(each_list)])
-        result_df['blockchain'] = blockchain
-        dft = pd.concat([dft, result_df]).reset_index(drop=True)
+    df.to_csv(data_file, index=False)
 
-    if dft.loc[dft['Date'] == dft['Date'].iloc[0], 'Blockchain'].unique().size < dft['Blockchain'].unique().size:
-        dft.drop(dft[dft['date'] == dft['Date'].iloc[0]].index, inplace = True)
-
-    if dft['Date'].iloc[0] > df['Date'].iloc[0]:
-        df = pd.concat([dft[~dft.date.isin(df['Date'])], df], ignore_index=True)
-        df.to_csv(data_file, index=False)
+if df.loc[df['Date'] == df['Date'].iloc[0], 'Blockchain'].unique().size < df['Blockchain'].unique().size:
+    df.drop(df[df['Date'] == df['Date'].iloc[0]].index, inplace = True)
 
 # Time Frame
 option_dates = st.slider(
     '**Date Range**',
-    min_value=datetime.strptime('2021-01-01', '%Y-%m-%d').date(),
+    min_value=datetime.strptime(df['Date'].min(), '%Y-%m-%d').date(),
     max_value=datetime.strptime(df['Date'].max(), '%Y-%m-%d').date(),
-    value=(datetime.strptime('2023-01-01', '%Y-%m-%d').date(), datetime.strptime(df['Date'].max(), '%Y-%m-%d').date()),
+    value=(datetime.strptime(str(date.today() - timedelta(90)), '%Y-%m-%d').date(), datetime.strptime(df['Date'].max(), '%Y-%m-%d').date()),
     key='option_dates'
 )
 
 df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-df = df.query("Date >= @st.session_state.option_dates[0] & Date <= @st.session_state.option_dates[1]")
+df = df.query("Blockchain == @option_blockchains & Date >= @option_dates[0] & Date <= @option_dates[1]")
 
 # Charts
-title = charts.query("Segment == @st.session_state.option_segments & Metric == @st.session_state.option_metrics")['Title'].iloc[0]
-y_axis = charts.query("Segment == @st.session_state.option_segments & Metric == @st.session_state.option_metrics")['Y Axis'].iloc[0]
+if len(option_blockchains) <= 1:
+    st.warning('Please select at least 2 blockchains to see the metrics.')
 
-fig = px.line(df, x='Date', y='Values', color='Blockchain', custom_data=['Blockchain'], title=f"{title} Over Time")
-fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title=y_axis, hovermode='x unified')
-fig.update_traces(hovertemplate='%{customdata}: %{y:,.0f}<extra></extra>')
-st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+else:
+    title = charts.query("Segment == @st.session_state.option_segments & Metric == @st.session_state.option_metrics")['Title'].iloc[0]
+    y_axis = charts.query("Segment == @st.session_state.option_segments & Metric == @st.session_state.option_metrics")['Y Axis'].iloc[0]
 
-c1, c2 = st.columns([2, 1])
-with c1:
+    fig = px.line(df, x='Date', y='Values', color='Blockchain', custom_data=['Blockchain'], title=f"{title} Over Time")
+    fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title=y_axis, hovermode='x unified')
+    fig.update_traces(hovertemplate='%{customdata}: %{y:,.0f}<extra></extra>')
+    st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
     fig = go.Figure()
     for i in option_blockchains:
         fig.add_trace(go.Scatter(
@@ -115,12 +100,14 @@ with c1:
         ))
     fig.update_layout(title=f'Share of {title} Over Time')
     st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-with c2:
-    fig = px.pie(df, values='Values', names='Blockchain', title=f"Share of {title}")
-    fig.update_layout(legend_title=None, legend_y=0.5)
-    fig.update_traces(textinfo='percent+label', textposition='inside')
-    st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
+    df_heatmap = df.copy()
+    df_heatmap['Normalized'] = df.groupby('Blockchain')['Values'].transform(lambda x: (x - x.min()) / (x.max() - x.min()))
+    fig = px.density_heatmap(df_heatmap, x='Blockchain', y=df_heatmap.Date.dt.strftime('%A'), z='Normalized', histfunc='avg', title=f"Heatmap of Normalized {title}")
+    fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title=None, coloraxis_colorbar=dict(title='Normalized'))
+    fig.update_xaxes(categoryorder='category ascending')
+    fig.update_yaxes(categoryorder='array', categoryarray=week_days)
+    st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
 # Whitespace
 st.write("""
